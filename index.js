171cdcbe338da77b31ef86f5c7aebea2e67e32bc
Dipam Sen <dipamsen.fp@gmail.com>
2024-02-13 00:25:34 +0530

    fix

--- index.js
+++ index.js
@@ -1,49 +1,49 @@
-const express = require("express")
-const cors = require("cors")
-const helmet = require("helmet")
-const morgan = require("morgan")
-const { default: axios } = require("axios")
-require("dotenv").config()
-const app = express()
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const { default: axios } = require("axios");
+require("dotenv").config();
+const app = express();
 
-app.use(cors())
-app.use(express.json())
-app.use(helmet())
-app.use(morgan("tiny"))
+app.use(cors());
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("tiny"));
 
 app.get("/", (req, res) => {
   res.send({
-    send: "Discord~"
-  })
-})
+    send: "Discord~",
+  });
+});
 
 const config = {
   headers: {
-    Authorization: `Bot ${process.env.BOT_TOKEN}`
-  }
-}
+    Authorization: `Bot ${process.env.BOT_TOKEN}`,
+  },
+};
 
-const url = `https://discord.com/api/channels/${process.env.DM_CHANNEL}/messages`
+const url = `https://discord.com/api/v10/channels/${process.env.DM_CHANNEL}/messages`;
 
 app.get("/sendmessage", async (req, res) => {
-  const { content } = req.query
-  if (!content) return res.status(400).json({ error: "No Content found" })
+  const { content } = req.query;
+  if (!content) return res.status(400).json({ error: "No Content found" });
   try {
-    const { data } = await axios.post(url, { content }, config)
-    res.json(data)
+    const { data } = await axios.post(url, { content }, config);
+    res.json(data);
   } catch (e) {
-    res.status(500).send(e)
+    res.status(500).send(e);
   }
-})
+});
 
 app.post("/sendmessage", async (req, res) => {
   try {
-    const { data } = await axios.post(url, req.body, config)
-    res.json(data)
+    const { data } = await axios.post(url, req.body, config);
+    res.json(data);
   } catch (e) {
-    res.status(400).send(e)
+    res.status(400).send(e);
   }
-})
+});
 
-const port = process.env.PORT || 2034
-app.listen(port, () => console.log(`Listening on ${port}`))
+const port = process.env.PORT || 2034;
+app.listen(port, () => console.log(`Listening on ${port}`));
